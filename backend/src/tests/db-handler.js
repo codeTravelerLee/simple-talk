0cd770d06a2f0d4b1c94e129652e79134f7c4220
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-12-26 02:55:00 +0000

    feat: 회원가입 테스트코드 작성

---
+++ backend/src/tests/db-handler.js
@@ -0,0 +1,36 @@
+//테스팅을 위한 가상DB(mongodb-memory-server) 설정 파일
+
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoTestDB;
+
+//테스트 수행전 가상 DB연결
+export const connect = async () => {
+  mongoTestDB = await MongoMemoryServer.create();
+  const uri = mongoTestDB.getUri();
+
+  await mongoose.connect(uri);
+};
+
+//테스트 전체 종료후 가상 DB연결 해제
+export const disConncet = async () => {
+  if (mongoTestDB) {
+    await mongoose.connection.dropDatabase(); //생성한 DB삭제
+    await mongoose.connection.close(); //mongoose와 테스트DB연결 해제
+    await mongoTestDB.stop(); //테스트DB프로세스 kill
+  }
+};
+
+//개별기능 테스트 후 DB데이터만 삭제, 스키마는 유지
+export const clearDb = async () => {
+  if (mongoTestDB) {
+    const collections = mongoose.connection.collections;
+
+    //key는 각 컬렉션(테이블)의 이름
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
+    }
+  }
+};
